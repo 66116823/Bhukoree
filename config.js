00114cee364f0c5b00a1fd189cf66432b24464ce
Pharadon <nakzataleebun2547@gmail.com>
2024-12-25 10:32:49 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// File config.js
+var config = {};
+config.mysql = {};
+config.mysql.host = 'localhost';
+config.mysql.port = 3306;
+config.mysql.database = 'bsc_hospital';
+config.mysql.user = 'root';
+config.mysql.password = '1234';
+config.express = {};
+config.express.port = 3001;
+module.exports = config;
